74062ddd7da70323105b6cc6978e04a1ca8a0e76
brendanwallace <brendanwallace@brendanwallace-glaptop0.roam.corp.google.com>
2016-11-07 22:28:41 -0800

    Use auth views

--- bull_bear/urls.py
+++ bull_bear/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
+	urls.url('^', urls.include('django.contrib.auth.urls')),
     urls.url(r'^$', views.index, name='index'),
     urls.url(r'^event/(?P<event_id>[0-9]+)/$', views.event, name='event'),
     urls.url(r'^user/(?P<user_id>[0-9]+)/$', views.user, name='user'),
